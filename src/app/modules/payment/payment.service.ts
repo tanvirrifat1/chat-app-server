50287637bbcd85228ff3d47518d31eb178cc605e
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-09-05 15:48:32 +0600

    added payment

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -3,6 +3,9 @@ import ApiError from '../../../errors/ApiError';
 import { Product } from '../product/product.model';
 import { IPayment } from './payment.interface';
 import { stripe } from '../../../shared/stripe';
+import { Payment } from './payment.model';
+import Stripe from 'stripe';
+import { Types } from 'mongoose';
 
 const createCheckoutSession = async (payload: IPayment) => {
   const isExistProduct = await Product.findById(payload.product);
@@ -46,6 +49,49 @@ const createCheckoutSession = async (payload: IPayment) => {
   }
 };
 
+const handleStripeWebhookService = async (event: Stripe.Event) => {
+  switch (event.type) {
+    case 'checkout.session.completed': {
+      const session = event.data.object as Stripe.Checkout.Session;
+
+      const { amount_total, metadata, payment_intent, status } = session;
+      const userId = metadata?.user as string;
+      const product = metadata?.product;
+      const email = session.customer_email || '';
+
+      const amountTotal = (amount_total ?? 0) / 100;
+
+      const paymentRecord = new Payment({
+        amount: amountTotal,
+        user: new Types.ObjectId(userId),
+        product: new Types.ObjectId(product),
+        email,
+        transactionId: payment_intent,
+        status,
+      });
+
+      await paymentRecord.save();
+      break;
+    }
+
+    case 'checkout.session.async_payment_failed': {
+      const session = event.data.object as Stripe.Checkout.Session;
+      const { payment_intent } = session;
+      console.log(payment_intent);
+      const payment = await Payment.findOne({ transactionId: payment_intent });
+      if (payment) {
+        payment.status = 'failed';
+        await payment.save();
+      }
+      break;
+    }
+
+    default:
+      console.log(`Unhandled event type: ${event.type}`);
+  }
+};
+
 export const PaymentService = {
   createCheckoutSession,
+  handleStripeWebhookService,
 };
