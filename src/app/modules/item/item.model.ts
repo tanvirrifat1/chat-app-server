8879bbdb87673cb7c4e875089b319615d622d2a9
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-09-26 15:09:03 +0600

    added mire

---
+++ src/app/modules/item/item.model.ts
@@ -0,0 +1,37 @@
+import { model, Schema } from 'mongoose';
+import { IItems } from './item.interface';
+
+const itemSchema = new Schema<IItems>(
+  {
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Item = model<IItems>('Item', itemSchema);
